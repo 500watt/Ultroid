4bf052da7f9d1c9168708955e40aaa4ff8265dea
500watt <85592814+500watt@users.noreply.github.com>
2021-06-09 07:54:23 +0700

    Update _inline.py

--- plugins/_inline.py
+++ plugins/_inline.py
@@ -22,7 +22,7 @@ from . import *
 
 # ================================================#
 notmine = f"This bot is for {OWNER_NAME}"
-ULTROID_PIC = "https://telegra.ph/file/115f149ed8e154641708b.jpg"
+ULTROID_PIC = "https://i.ibb.co/qybc2mL/IMG-20210609-073353-612.jpg"
 helps = get_string("inline_1")
 
 add_ons = udB.get("ADDONS")
@@ -56,8 +56,8 @@ async def e(o):
         )
         res = [
             await b.article(
-                title="Ultroid Userbot",
-                url="https://t.me/TeamUltroid",
+                title="My Assisatant",
+                url="",
                 description="Userbot | Telethon ",
                 text=ALIVEMSG,
                 thumb=InputWebDocument(ULTROID_PIC, 0, "image/jpeg", []),
@@ -66,13 +66,13 @@ async def e(o):
                     [
                         Button.url(
                             text="Repo",
-                            url="https://github.com/Teamultroid/Ultroid",
+                            url="https://github.com/",
                         ),
                     ],
                 ],
             ),
         ]
-        await o.answer(res, switch_pm=f"👥 ULTROID PORTAL", switch_pm_param="start")
+        await o.answer(res, switch_pm=f"👥 PORTAL", switch_pm_param="start")
 
 
 @in_pattern("ultd")
@@ -447,7 +447,7 @@ async def on_plug_in_callback_query_handler(event):
         reply_pop_up_alert = f"{plugin_name} has no detailed help..."
     else:
         reply_pop_up_alert = help_string
-    reply_pop_up_alert += "\n© @TeamUltroid"
+    reply_pop_up_alert += "\n© My Assistant"
     buttons = [
         [
             Button.inline(
